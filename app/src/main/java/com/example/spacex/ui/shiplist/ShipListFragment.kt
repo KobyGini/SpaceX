a9ef6f13e1ff2ae9974d553c275d3c14e1cedc5d
kobykorsia <kobyk@gini-apps.co.il>
2021-11-24 13:43:33 +0200

    view binding

--- app/src/main/java/com/example/spacex/ui/shiplist/ShipListFragment.kt
+++ app/src/main/java/com/example/spacex/ui/shiplist/ShipListFragment.kt
@@ -1,41 +1,58 @@
 package com.example.spacex.ui.shiplist
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.paging.ExperimentalPagingApi
-import androidx.recyclerview.widget.RecyclerView
-import com.example.spacex.R
+import com.example.spacex.databinding.FragmentShipListBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ShipListFragment : Fragment(R.layout.fragment_ship_list){
+class ShipListFragment : Fragment(){
 
     private val shipListViewModel: ShipListViewModel by viewModels()
-    private lateinit var launchRecyclerView: RecyclerView
-    lateinit var adapter: ShipListPagingAdapter
+    private val adapter = ShipListPagingAdapter()
+
+    private var _binding: FragmentShipListBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentShipListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
     @OptIn(ExperimentalPagingApi::class)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setUpRecyclerView(view)
+        setUpRecyclerView()
 
         shipListViewModel.getShipListLive().observe(viewLifecycleOwner, {
             adapter.submitData(lifecycle, it)
         })
     }
 
-    private fun setUpRecyclerView(view: View) {
-        launchRecyclerView = view.findViewById(R.id.launch_recyclerview)
-        adapter = ShipListPagingAdapter()
+    private fun setUpRecyclerView() {
         adapter.setOnLaunchClickListener{
             val action = ShipListFragmentDirections
                     .actionShipListFragmentToShipDetailsFragment(it.shipId)
             findNavController().navigate(action)
         }
-        launchRecyclerView.adapter = adapter
+        binding.shipListRecyclerview.adapter = adapter
     }
 }
--- app/src/main/java/com/example/spacex/util/BundleConstants.kt
+++ app/src/main/java/com/example/spacex/util/BundleConstants.kt
@@ -5,4 +5,5 @@ object BundleConstants {
     const val LAUNCH_ID = "launchId"
     const val LAUNCH_NAME = "launchName"
     const val LAUNCH_DETAILS = "launchDetails"
+    const val SHIP_ID = "shipId"
 }
