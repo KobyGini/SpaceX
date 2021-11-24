5b5376341d05aeadfc0ae447d785091d3aa20a78
kobykorsia <kobyk@gini-apps.co.il>
2021-11-24 12:06:54 +0200

    worker update

--- app/src/main/java/com/example/spacex/MainActivity.kt
+++ app/src/main/java/com/example/spacex/MainActivity.kt
@@ -1,9 +1,5 @@
 package com.example.spacex
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
@@ -15,8 +11,6 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 
 import com.example.spacex.databinding.ActivityMainBinding
-import com.example.spacex.util.Constants
-import com.example.spacex.util.Constants.CHANNEL_ID
 import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -50,8 +44,6 @@ class MainActivity : AppCompatActivity() {
 
         navView.setupWithNavController(navController)
 
-        createNotificationChannel()
-
         mainViewModel.updateApiContent()
     }
 
@@ -60,20 +52,4 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
-    private fun createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = Constants.CHANNEL_NAME
-            val descriptionText = Constants.CHANNEL_DESCRIPTION
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-                description = descriptionText
-            }
-            // Register the channel with the system
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
 }
--- app/src/main/java/com/example/spacex/worker/HiltSchedulerWorker.kt
+++ app/src/main/java/com/example/spacex/worker/HiltSchedulerWorker.kt
@@ -1,18 +1,15 @@
 package com.example.spacex.worker
 
-import android.app.PendingIntent
 import android.content.Context
 import android.os.Bundle
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.hilt.work.HiltWorker
-import androidx.navigation.NavDeepLinkBuilder
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.example.spacex.MainActivity
-import com.example.spacex.R
 import com.example.spacex.repository.Repository
-import com.example.spacex.util.Constants
+import com.example.spacex.util.BundleConstants.LAUNCH_DETAILS
+import com.example.spacex.util.BundleConstants.LAUNCH_ID
+import com.example.spacex.util.BundleConstants.LAUNCH_NAME
+import com.example.spacex.util.NotificationHelper
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.Dispatchers
@@ -25,11 +22,7 @@ class HiltSchedulerWorker @AssistedInject constructor(
     val repository: Repository
 ) : CoroutineWorker(appContext, workerParams) {
 
-    private var builder = NotificationCompat.Builder(appContext, Constants.CHANNEL_ID)
-        .setSmallIcon(R.drawable.ic_launch_rocket)
-        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-    private val notificationId = 12543
+    private val notificationHelper = NotificationHelper()
 
     override suspend fun doWork(): Result {
 
@@ -38,24 +31,11 @@ class HiltSchedulerWorker @AssistedInject constructor(
             val newLaunches = repository.updateContents()
             newLaunches?.let {
                 if (it.isNotEmpty()) {
-
                     val bundle = Bundle()
-                    bundle.putString("launchId",it.last().id)
-                    val pendingIntent: PendingIntent = NavDeepLinkBuilder(appContext)
-                        .setComponentName(MainActivity::class.java)
-                        .setGraph(R.navigation.nav_graph)
-                        .setDestination(R.id.launchDetailsFragment)
-                        .setArguments(bundle)
-                        .createPendingIntent()
-
-                    builder.setContentTitle(it.last().missionName)
-                        .setContentText(it.last().details)
-                        .setContentIntent(pendingIntent)
-
-                    with(NotificationManagerCompat.from(appContext)) {
-                        // notificationId is a unique int for each notification that you must define
-                        notify(notificationId, builder.build())
-                    }
+                    bundle.putString(LAUNCH_ID,it.last().id)
+                    bundle.putString(LAUNCH_NAME,it.last().missionName)
+                    bundle.putString(LAUNCH_DETAILS,it.last().details)
+                    notificationHelper.createNavDeepLinkToLaunchDetails(appContext,bundle)
                 }
             }
         }
